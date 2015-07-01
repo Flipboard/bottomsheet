923dfadee4b165528c1f863678c1c107bc0b98dd
Henri Sweers <pandanomic@gmail.com>
2015-07-01 02:33:59 -0700

    Add MenuActivity to sample
    This is an example of the new MenuSheetView

--- bottomsheet-sample/src/main/java/com/flipboard/bottomsheet/sample/MainActivity.java
+++ bottomsheet-sample/src/main/java/com/flipboard/bottomsheet/sample/MainActivity.java
@@ -22,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.menu_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MenuActivity.class));
             }
+        });
     }
 }
---
+++ bottomsheet-sample/src/main/java/com/flipboard/bottomsheet/sample/MenuActivity.java
@@ -0,0 +1,54 @@
+package com.flipboard.bottomsheet.sample;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.flipboard.bottomsheet.BottomSheetLayout;
+import com.flipboard.bottomsheet.R;
+import com.flipboard.bottomsheet.commons.MenuSheetView;
+
+/**
+ * Activity demonstrating the use of {@link MenuSheetView}
+ */
+public class MenuActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        bottomSheetLayout = (BottomSheetLayout) findViewById(R.id.bottomsheet);
+        findViewById(R.id.list_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMenuSheet(MenuSheetView.MenuType.LIST);
+            }
+        });
+        findViewById(R.id.grid_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMenuSheet(MenuSheetView.MenuType.GRID);
+            }
+        });
+    }
+
+    private void showMenuSheet(MenuSheetView.MenuType menuType) {
+        MenuSheetView menuSheetView = new MenuSheetView.Builder(MenuActivity.this)
+                .setTitle("Create...")
+                .setMenuType(menuType)
+                .setMenuRes(R.menu.create)
+                .setOnMenuItemClickListener(new MenuSheetView.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MenuActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        if (bottomSheetLayout.isSheetShowing()) {
+                            bottomSheetLayout.dismissSheet();
+                        }
+                        return true;
+                    }
+                })
+                .create();
+        bottomSheetLayout.showWithSheetView(menuSheetView);
+    }
+}
