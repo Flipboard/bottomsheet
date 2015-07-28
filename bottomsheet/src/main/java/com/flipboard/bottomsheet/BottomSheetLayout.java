57bbe7bbd5b0f91e8aaff548251782ab69b5a491
Davide Steduto <dave.dna@gmail.com>
2015-07-29 01:16:32 +0200

    Added custom peek and refactored boolean methods

--- bottomsheet/src/main/java/com/flipboard/bottomsheet/BottomSheetLayout.java
+++ bottomsheet/src/main/java/com/flipboard/bottomsheet/BottomSheetLayout.java
@@ -91,6 +91,7 @@ public class BottomSheetLayout extends FrameLayout {
     private boolean interceptContentTouch = true;
     private int currentSheetViewHeight;
     private boolean hasIntercepted;
+    private float peek;
 
     /** Snapshot of the touch's y position on a down event */
     private float downY;
@@ -132,6 +133,8 @@ public class BottomSheetLayout extends FrameLayout {
         dimView = new View(getContext());
         dimView.setBackgroundColor(Color.BLACK);
         dimView.setAlpha(0);
+
+        peek = 0;//getHeight() return 0 at start!
 
         setFocusableInTouchMode(true);
     }
@@ -504,7 +507,15 @@ public class BottomSheetLayout extends FrameLayout {
      * @return The peeked state translation for the presented sheet view. Translation is counted from the bottom of the view.
      */
     public float getPeekSheetTranslation() {
-        return hasFullHeightSheet() ?  getHeight() / 3 : getSheetView().getHeight();
+        return peek == 0 ? (hasFullHeightSheet() ? getHeight() / 3 : getSheetView().getHeight()) : peek;
+    }
+
+    /**
+     * Set custom height for PEEKED state.
+     * @param peek
+     */
+    public void setPeekSheetTranslation(float peek) {
+        this.peek = peek;
     }
 
     /**
@@ -665,7 +676,7 @@ public class BottomSheetLayout extends FrameLayout {
      * Returns the current peekOnDismiss value, which controls the behavior response to back presses
      * when the current state is {@link State#EXPANDED}.
      */
-    public boolean getPeekOnDismiss() {
+    public boolean isPeekOnDismiss() {
         return peekOnDismiss;
     }
 
@@ -683,7 +694,7 @@ public class BottomSheetLayout extends FrameLayout {
      * @return true if we are intercepting content view touches or false to allow interaction with
      * Bottom Sheet's content view. Default value is true.
      */
-    public boolean getInterceptContentTouch() {
+    public boolean isInterceptContentTouch() {
         return interceptContentTouch;
     }
 
