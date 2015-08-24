6eb1044547db6e2e4db6ce0da2b429e6c7cc67f4
Zac Sweers <pandanomic@gmail.com>
2015-08-23 22:54:57 -0700

    Implement ImagePickerSheetView
    This is a sheet view that shows a grid of recent local images as well as options to take a new picture or pick one from any app that supports the image picking API. This adds a dependency on the actual bottom sheet library (to access its isTablet/predictedDefaultWidth functions), which we should probably have been doing anyway.

---
+++ bottomsheet-commons/src/main/java/com/flipboard/bottomsheet/commons/SquareImageView.java
@@ -0,0 +1,35 @@
+package com.flipboard.bottomsheet.commons;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * An ImageView that keeps a 1:1 aspect ratio
+ */
+final class SquareImageView extends ImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @SuppressWarnings("SuspiciousNameCombination")
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
