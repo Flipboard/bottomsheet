b275d79dd1515080f209866c7ce9cd10ef381d7b
Zac Sweers <zsweers@uber.com>
2015-11-20 21:52:45 -0800

    Standardize util class

--- bottomsheet-commons/src/main/java/android/support/v4/app/AccessFragmentInternals.java
+++ bottomsheet-commons/src/main/java/android/support/v4/app/AccessFragmentInternals.java
@@ -1,7 +1,8 @@
 package android.support.v4.app;
 
-public class AccessFragmentInternals {
+public final class AccessFragmentInternals {
     private AccessFragmentInternals() {
+        throw new AssertionError("No instances.");
     }
 
     public static int getContainerId(Fragment fragment) {
