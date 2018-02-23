Flipboard/bottomsheet

commit c3d365a648b9c17d64e929c03d94825059053dec

Fix wrong bottom sheet position after sheet gets expanded
We need to call setSheetTranslation after layout expand events as well as layout shrink

bottomsheet/src/main/java/com/flipboard/bottomsheet/BottomSheetLayout.java
@@ -674,12 +674,16 @@ public class BottomSheetLayout extends FrameLayout {
             @Override
             public void onLayoutChange(View sheetView, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
                 int newSheetViewHeight = sheetView.getMeasuredHeight();
-                if (state != State.HIDDEN && newSheetViewHeight < currentSheetViewHeight) {
+                if (state != State.HIDDEN) {
                     // The sheet can no longer be in the expanded state if it has shrunk
-                    if (state == State.EXPANDED) {
+                    if (newSheetViewHeight < currentSheetViewHeight && state == State.EXPANDED) {
                         setState(State.PEEKED);
+                    } else if (newSheetViewHeight > currentSheetViewHeight && newSheetViewHeight == getMaxSheetTranslation() && state == State.PEEKED) {
+                        setState(State.EXPANDED);
                     }
-                    setSheetTranslation(newSheetViewHeight);
+                    if (newSheetViewHeight != currentSheetViewHeight) {
+                        setSheetTranslation(newSheetViewHeight);
+                    }
                 }
                 currentSheetViewHeight = newSheetViewHeight;
             }

commit 85024ab6170b0f18b8c307e5ced4836af4a05594

Fixes faulty bottom sheet position
Caused by: https://github.com/Flipboard/bottomsheet/commit/c3d365a648b9c17d64e9$
Peek to expand case incorrectly triggered during bottomsheet's initial layout

bottomsheet/src/main/java/com/flipboard/bottomsheet/BottomSheetLayout.java
@@ -676,12 +676,15 @@ public class BottomSheetLayout extends FrameLayout {
                 int newSheetViewHeight = sheetView.getMeasuredHeight();
                 if (state != State.HIDDEN) {
                     // The sheet can no longer be in the expanded state if it has shrunk
-                    if (newSheetViewHeight < currentSheetViewHeight && state == State.EXPANDED) {
-                        setState(State.PEEKED);
-                    } else if (newSheetViewHeight > currentSheetViewHeight && newSheetViewHeight == getMaxSheetTranslation() && state == State.PEEKED) {
-                        setState(State.EXPANDED);
-                    }
-                    if (newSheetViewHeight != currentSheetViewHeight) {
+                    if (newSheetViewHeight < currentSheetViewHeight) {
+                        if (state == State.EXPANDED) {
+                            setState(State.PEEKED);
+                        }
+                        setSheetTranslation(newSheetViewHeight);
+                    } else if (currentSheetViewHeight > 0 && newSheetViewHeight > currentSheetViewHeight && state == State.PEEKED) {
+                        if (newSheetViewHeight == getMaxSheetTranslation()) {
+                            setState(State.EXPANDED);
+                        }
                         setSheetTranslation(newSheetViewHeight);
                     }
                 }